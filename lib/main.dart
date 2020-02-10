0941936783bd0f07b7e1517bdbe0c53840d03ccc
Yong Joseph Bakos <ybakos@humanoriented.com>
2020-02-09 22:29:39 -0800

    Main: Demonstrate Builder.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'orientation_builder_demo.dart';
 
 void main() {
   SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp, DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight
   ]);
   runApp(App());
 }
 
 class App extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,9 +18,29 @@ class App extends StatelessWidget {
         theme: ThemeData(primarySwatch: Colors.blue),
         home: Scaffold(
           appBar: AppBar(title: Text('Adaptive Layouts')),
-          body: OrientationBuilderDemo()
+          body: Center(child: Text('Builder Demo')),
+
+          // floatingActionButton: FloatingActionButton(onPressed: () {
+          //   Scaffold.of(context).showSnackBar(SnackBar(content: Text('SNAAAAACKS!')));
+          //   })
+
+          // floatingActionButton: SnackFab(),
+          floatingActionButton: Builder(builder: (context) {
+            return FloatingActionButton(onPressed: () {
+              Scaffold.of(context)
+                  .showSnackBar(SnackBar(content: Text('SNAAAAACKS!')));
+            });
+          }),
         )
       );
   }
-  
+}
+
+class SnackFab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(onPressed: () {
+        Scaffold.of(context).showSnackBar(SnackBar(content: Text('SNAAAAACKS!')));
+      });
+  }
 }
