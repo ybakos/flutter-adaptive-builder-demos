d54ee2634b92a23b310c1beeef1ab0a737296d8c
Yong Joseph Bakos <ybakos@humanoriented.com>
2020-02-09 22:31:26 -0800

    LayoutBuilder demo.

--- lib/main.dart
+++ lib/main.dart
@@ -3,44 +3,51 @@ import 'package:flutter/services.dart';
 
 void main() {
   SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.landscapeLeft,
-    DeviceOrientation.landscapeRight
+    DeviceOrientation.portraitUp, DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight
   ]);
   runApp(App());
 }
 
 class App extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Adaptive Layouts',
         theme: ThemeData(primarySwatch: Colors.blue),
+ 
         home: Scaffold(
           appBar: AppBar(title: Text('Adaptive Layouts')),
-          body: Center(child: Text('Builder Demo')),
-
-          // floatingActionButton: FloatingActionButton(onPressed: () {
-          //   Scaffold.of(context).showSnackBar(SnackBar(content: Text('SNAAAAACKS!')));
-          //   })
-
-          // floatingActionButton: SnackFab(),
-          floatingActionButton: Builder(builder: (context) {
-            return FloatingActionButton(onPressed: () {
-              Scaffold.of(context)
-                  .showSnackBar(SnackBar(content: Text('SNAAAAACKS!')));
-            });
-          }),
-        )
+          body: LayoutBuilder(builder: layoutDecider)
+          // body: Container(
+          //   constraints: BoxConstraints(minWidth: 400, minHeight: 300, maxWidth: 400, maxHeight: 300),
+          //   child: LayoutBuilder(builder: layoutDecider)
+          // ),
+          )
       );
+  }
+  
+  Widget layoutDecider(BuildContext context, BoxConstraints constraints) =>
+    constraints.maxWidth < 500 ? VerticalLayout() : HorizontalLayout();
+
+}
+
+class VerticalLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.lightGreen);
   }
 }
 
-class SnackFab extends StatelessWidget {
+class HorizontalLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton(onPressed: () {
-        Scaffold.of(context).showSnackBar(SnackBar(content: Text('SNAAAAACKS!')));
-      });
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Expanded(child: Container(color: Colors.lightGreen)),
+        Expanded(child: Container(color: Colors.deepOrange))
+      ]);
   }
+
 }
